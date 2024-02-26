1860bcc4d472916c48010d20f4071815e7d3b514
Hinson Li <contactme@hinsonli.com>
2024-02-26 20:45:28 +1100

    Update bulk_tag_cw_log_group.sh

--- automation_scripts/bulk_tag_cw_log_group.sh
+++ automation_scripts/bulk_tag_cw_log_group.sh
@@ -7,11 +7,14 @@ TAG_KEY2="Key2"
 TAG_VALUE2="Value2"
 # Add more tags as needed
 
-# List log groups and filter by name containing "string", then tag them
-aws logs describe-log-groups --query 'logGroups[?contains(logGroupName, `string`)].logGroupName' --output text | while IFS=$'\t' read -r logGroupName; do
+# Get list of log group names containing "string"
+readarray -t logGroupNames < <(aws logs describe-log-groups --query 'logGroups[?contains(logGroupName, `string`)].logGroupName' --output text)
+
+# Tag each log group individually
+for logGroupName in "${logGroupNames[@]}"; do
     echo "Tagging log group: $logGroupName"
-    aws logs tag-log-group --log-group-name "$logGroupName" --tags "$TAG_KEY1"="$TAG_VALUE1" "$TAG_KEY2"="$TAG_VALUE2"
-    # Repeat --tags for each additional tag
+    aws logs tag-log-group --log-group-name "$logGroupName" --tags "$TAG_KEY1"="$TAG_VALUE1"
+    # Add additional tags as needed
 done
 
 echo "Tagging complete."
