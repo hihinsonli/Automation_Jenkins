cf6d125e7d1f84c72fd76bb64af7b5d4df5a7a53
Hinson Li <contactme@hinsonli.com>
2024-02-26 20:29:33 +1100

    Create bulk_tag_cw_log_group.sh

---
+++ automation_scripts/bulk_tag_cw_log_group.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Declare a 
+declare -A TAGS=(
+    ["Key1"]="Value1"
+    ["Key2"]="Value2"
+    ["Key3"]="Value3"
+)
+
+# Convert associative array to a string of tags for AWS CLI
+TAGS_STR=""
+for key in "${!TAGS[@]}"; do
+    TAGS_STR="$TAGS_STR$key=${TAGS[$key]} "
+done
+
+# Trim trailing space
+TAGS_STR=$(echo "$TAGS_STR" | xargs)
+
+# List log groups and filter by name containing "ApplicationLogs", then tag them
+aws logs describe-log-groups --query 'logGroups[?contains(logGroupName, `ApplicationLogs`)].logGroupName' --output text | while read -r logGroupName; do
+    echo "Tagging log group: $logGroupName with tags: $TAGS_STR"
+    aws logs tag-log-group --log-group-name "$logGroupName" --tags $TAGS_STR
+done
+
+echo "Tagging complete."
